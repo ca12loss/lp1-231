96a9ac8020b64dbca43c22e4f8d11e51d4a5c45b
Carlos Alberto <alberto.carlos@aluno.ifsp.edu.br>
2023-06-13 22:12:50 -0300

    It´s hard to get around the wind

--- src/semana14/Exercicios/AquarioTest.java
+++ src/semana14/Exercicios/AquarioTest.java
@@ -1,22 +1,96 @@
 package semana14.Exercicios;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
  
 public class AquarioTest {
-    @Test
     
-    public void testargetesetComprimento(){
-        Aquario aquario = new Aquario(5,5,5);
-        aquario.setComprimento(10);
-        aquario.setAltura(10);
-        aquario.setLargura(10);
+@Test
+    public void testsetComprimentoComValorPositivo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+
+        aquario.setComprimento(6.0);
+
+        assertEquals(6.0,aquario.getComprimento());
         
-        assertEquals(10, aquario.getComprimento());
-        assertEquals(10, aquario.getAltura());
-        assertEquals(10, aquario.getLargura());
     }
+
+@Test 
+    public void testsetAlturaComValorPositivo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+
+        aquario.setAltura(9.0);
+
+        assertEquals(9.0,aquario.getAltura());
+    }
+
+@Test
+    public void testsetLarguraComValorPositivo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+
+        aquario.setLargura(14.0);
+
+        assertEquals(14.0,aquario.getLargura());
+    }
+
+@Test
+    public void testsetComprimentoComValorNegativo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+        
+        assertThrows(RuntimeException.class, () -> {
+            aquario.setComprimento(-10.0);
+        });
+    }
+
+@Test
+        public void testsetAlturaComValorNegativo(){
+            Aquario aquario = new Aquario(10.0,5.0,7.0);
+            
+            assertThrows(RuntimeException.class, () -> {
+                aquario.setAltura(-5.0);
+            });
+    
+    }
+
+@Test
+    public void testsetLarguraComValorNegativo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+        
+        assertThrows(RuntimeException.class, () -> {
+            aquario.setLargura(-7.0);
+        });
+
+}
+
+@Test
+    public void testsetComprimentoComValorNulo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+        
+        assertThrows(RuntimeException.class, () -> {
+            aquario.setComprimento(0.0);
+        });
+    }
+
+@Test
+        public void testsetAlturaComValorNulo(){
+            Aquario aquario = new Aquario(10.0,5.0,7.0);
+            
+            assertThrows(RuntimeException.class, () -> {
+                aquario.setAltura(0.0);
+            });
+    
+    }
+
+@Test
+    public void testsetLarguraComValorNulo(){
+        Aquario aquario = new Aquario(10.0,5.0,7.0);
+        
+        assertThrows(RuntimeException.class, () -> {
+            aquario.setLargura(0.0);
+        });
+
+}
 
    
 
